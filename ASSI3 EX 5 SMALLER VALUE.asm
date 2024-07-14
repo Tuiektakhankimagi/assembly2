60eb17af37b9ebc18e6c767c02629e87b931b6f3
Tuiektakhankimagi <udmadbaccha@gmail.com>
2024-07-14 22:32:50 +0530

    Update ASSI3 EX 5 SMALLER VALUE.asm

--- ASSI3 EX 5 SMALLER VALUE.asm
+++ ASSI3 EX 5 SMALLER VALUE.asm
@@ -1,9 +1,14 @@
-LXI H , 2501H
-MOV A, M
-LXI H, 2502H
-MOV B,M 
-CMP B
-JC LOOP
-MOV A, B
-LOOP:STA 2503H
-HLT
+LXI H, 2501H    ; Load HL pair with the memory address 2501H (pointing to the first byte)
+MOV A, M        ; Move the byte at memory location 2501H to accumulator A
+
+LXI H, 2502H    ; Load HL pair with the memory address 2502H (pointing to the second byte)
+MOV B, M        ; Move the byte at memory location 2502H to register B
+
+CMP B           ; Compare the byte in accumulator A with the byte in register B
+JC LOOP         ; Jump to LOOP if the byte in A is less than the byte in B
+
+MOV A, B        ; Move the byte in B to accumulator A (if not jumped)
+
+LOOP:STA 2503H       ; Store the byte in accumulator A to memory location 2503H
+
+HLT             ; Halt the program
