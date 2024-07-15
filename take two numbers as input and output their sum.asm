88ac6145d74f15788ec35a552d0dd86a68d9e89f
Tuiektakhankimagi <udmadbaccha@gmail.com>
2024-07-15 12:01:47 +0530

    Update take two numbers as input and output their sum.asm

--- take two numbers as input and output their sum.asm
+++ take two numbers as input and output their sum.asm
@@ -1,5 +1,5 @@
-MVI A,25H
-NVI B,IAH
-ADD B
-STA 3000H
-HLT
+MVI A, 25H      ; Load immediate value 25H into register A
+MVI B, 1AH      ; Load immediate value 1AH into register B
+ADD B           ; Add the value in register B to register A
+STA 3000H       ; Store the contents of register A to memory address 3000H
+HLT             ; Halt the program
